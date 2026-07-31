b7246793961d70e3b0e7fcde2f248b677a49b655
tomgeo444 <tomgeo110@gmail.com>
2026-07-31 14:06:18 +0530

    fixed spacing

--- src/database/insert.py
+++ src/database/insert.py
@@ -1,16 +1,22 @@
 from src.database.connection import get_connection
-def insert_user(username,email,password_hash,role):
-    connection=get_connection()
-    cursor=connection.cursor()
-    query="""
-	INSERT INTO users (username,email,password_hash,role)
-	VALUES (%s,%s,%s,%s)
- 	"""
+
+
+def insert_user(username, email, password_hash, role):
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    query = """
+    INSERT INTO users (username, email, password_hash, role)
+    VALUES (%s, %s, %s, %s)
+    """
+
     try:
-	cursor.execute(query, (username,email,password_hash,role))
-	connection.commit()
+        cursor.execute(query, (username, email, password_hash, role))
+        connection.commit()
+
     except Exception as e:
-	print(e)
+        print(e)
+
     finally:
-	cursor.close()
-	connection.close()
+        cursor.close()
+        connection.close()
